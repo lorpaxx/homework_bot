bd22615a63c3c9fa180fc8ca8c3864c6f115099c
Александр Бебякин <lorpaxx@yandex.ru>
2021-12-13 09:40:26 +0300

    перед рефакторингом

--- homework.py
+++ homework.py
@@ -3,24 +3,22 @@ import time
 import requests
 import logging
 import telegram
-from logging.handlers import RotatingFileHandler
+# from logging.handlers import RotatingFileHandler
+# from logging.handlers import StreamHandler
 from dotenv import load_dotenv
 from exceptions import APIAnsverWrongData
 from exceptions import CheckTokenException
 from exceptions import APIAnswerInvalidException
 
-logging.basicConfig(
-    level=logging.CRITICAL,
-    format='[%(asctime)s]-[%(name)s]-[%(levelname)s]-%(message)s'
-)
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
-handler = RotatingFileHandler(
-    'homework.log',
-    maxBytes=50000,
-    backupCount=5
-)
+# handler = RotatingFileHandler(
+#     'homework.log',
+#     maxBytes=50000,
+#     backupCount=5
+# )
+handler = logging.StreamHandler()
 logger.addHandler(handler)
 formatter = logging.Formatter(
     '[%(asctime)s]-[%(name)s]-[%(levelname)s]-%(message)s'
@@ -50,7 +48,7 @@ def send_message(bot, message):
     Отправляет сообщение ботом о домашеней работе.
     В соответствующий чат.
     """
-    logger.debug('send_message start')
+    logger.debug('send_message() start')
     try:
         bot.send_message(TELEGRAM_CHAT_ID, message)
         logger.info(f'Бот отправил сообщение "{message}"')
@@ -65,7 +63,6 @@ def get_api_answer(timestamp):
     timestamp - метка времени в формате UnixTime.
     """
     logger.debug('get_api_answer start')
-    logger.debug(timestamp)
     params = {'from_date': timestamp}
     logger.debug(params)
     response = requests.get(
@@ -73,12 +70,20 @@ def get_api_answer(timestamp):
         headers=HEADERS,
         params=params
     )
+    logger.debug(response)
     code = str(response.status_code)
-    logger.info('response status code ' + code)
+    logger.debug(
+        f'timestamp - {timestamp}, '
+        f'response status code {code} '
+    )
 
     if response.status_code != 200:
+        logger.error(
+            f'timestamp - {timestamp}, '
+            f'response status code {code} '
+        )
         raise APIAnswerInvalidException(
-            f'Некорректный ответ с сервера, статус код - {code}')
+            f'Неожиданный ответ с сервера, статус код - {code}')
     return response.json()
 
 
@@ -95,12 +100,18 @@ def check_response(response):
         logger.debug(homeworks)
         logger.info('В ответе ' + str(len(homeworks)) + ' домашних работ')
     else:
+        logger.error(
+            f'На вход функции подан тип "{type(response)}" вместо "dict"')
         raise TypeError(
             f'На вход функции подан тип "{type(response)}" вместо "dict"')
 
     if type(homeworks) is list:
+        logger.debug('homeworks is list')
         return homeworks
     else:
+        logger.error(
+            f'Список домашних работ имеет тип "{type(response)}" вместо "list"'
+        )
         raise TypeError(
             f'Список домашних работ имеет тип "{type(response)}" вместо "list"'
         )
@@ -111,28 +122,33 @@ def parse_status(homework):
     Ищем в словаре homework имя домашней работы и её статус.
     На выход выдаём текст для отправки.
     """
-    logger.debug('parse_status start')
+    logger.debug('parse_status() start')
     logger.debug(homework)
 
     homework_name = homework.get('homework_name')
-    logger.info(homework_name)
+    logger.debug(homework_name)
 
     if homework_name is None:
-        raise KeyError('Имени домашней работы нет в ответе от сервера')
+        logger.error('homework_name нет в ответе от сервера')
+        raise KeyError('homework_name нет в ответе от сервера')
 
     homework_status = homework.get('status')
-    logger.info(homework_name)
+    logger.debug(homework_status)
 
     if homework_status is None:
+        logger.error(
+            'homework_status нет в ответе от сервера')
         raise APIAnsverWrongData(
-            'Статуса домашенй работы нет в ответе от сервера')
+            'homework_status нет в ответе от сервера')
 
     verdict = HOMEWORK_STATUSES.get(homework_status)
-    logger.info(verdict)
+    logger.debug(verdict)
 
     if verdict is None:
+        logger.error(
+            'homework_status в ответе от сервера не опознан')
         raise APIAnsverWrongData(
-            'Статус домашней работы в ответе от сервера не опознан')
+            'homework_status в ответе от сервера не опознан')
 
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
@@ -142,7 +158,7 @@ def check_tokens():
     Проверяем наличие переменных окружения.
     Если хоть одна из них None -> False, иначе True.
     """
-    logger.debug('check_tokens start')
+    logger.debug('check_tokens() start')
     args = {
         'PRACTICUM_TOKEN': PRACTICUM_TOKEN,
         'TELEGRAM_TOKEN': TELEGRAM_TOKEN,
@@ -163,7 +179,7 @@ def check_tokens():
 
 def main():
     """Основная логика работы бота."""
-    logger.debug('main start')
+    logger.debug('main() start')
     if not check_tokens():
         raise CheckTokenException(
             "Отсутствует одна из обязательных переменных окружения")
@@ -171,18 +187,23 @@ def main():
     # при старте скрипта вычитаем список всех домашек
     current_timestamp = 0
     logger.debug('current_timestamp = 0')
+    homework_messages = set()
     while True:
         try:
             response = get_api_answer(current_timestamp)
             homeworks = check_response(response)
             for homework in homeworks:
                 message = parse_status(homework)
-                send_message(bot, message)
+                if message not in homework_messages:
+                    send_message(bot, message)
+                else:
+                    homework_messages.add(message)
 
             current_timestamp = int(time.time())
 
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
+            send_message(bot, message)
 
         finally:
             time.sleep(RETRY_TIME)
